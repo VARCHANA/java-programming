fa96b7fa1d2c1486faf1db97088542482e2abfd0
VARCHANA <43057131+VARCHANA@users.noreply.github.com>
2018-09-07 11:17:00 +0530

    Add files via upload

---
+++ mathemathical.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+    int a=3,b=4,c;
+    c=a+b;
+    System.out.println("add the two numbers"+c);
+    c=aem.out.println("subract the two number"+c);
+    c=a*b;
+    System.out.println("multiplication the two number"+c);
+    c=a/b;
+    System.out.println("division the two number"+c);
+  }
+}
