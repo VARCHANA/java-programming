ac0bc8202927d929fbba6499bb99842068ea049d
VARCHANA <43057131+VARCHANA@users.noreply.github.com>
2018-09-07 00:02:35 -0700

    Add files via upload

---
+++ float.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    int a=4;
+    float b=6,c;
+    c=a+b;
+    System.out.println("add the two numbers"+c);
+    c=a-b;
+    System.out.println("subract the two numbers"+c);
+    c=a*b;
+    System.out.println("multiplication the two numbers"+c);
+    c=a/b;
+    System.out.println("division the two numbers"+c);
+  }
+}
