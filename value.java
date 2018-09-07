2b48289a29907674b4fafbc60400bc426a1fef64
VARCHANA <43057131+VARCHANA@users.noreply.github.com>
2018-09-07 02:44:17 -0700

    Add files via upload

---
+++ value.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter the value");
+    int a=sc.nextInt();
+    int b=5;
+    int c=a/b;
+    System.out.println("add:"+c);
+  }
+}
