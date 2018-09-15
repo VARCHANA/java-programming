bceb417557663411f9698ab1279727ce180ca310
VARCHANA <43057131+VARCHANA@users.noreply.github.com>
2018-09-15 14:52:32 +0530

    Add files via upload

---
+++ string.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {
+  System.out.println("Hello world!");
+   // Sridevi details
+        String name = "sridevi";
+        String nickname = "devi";
+        String city = "chennai";
+        int age = 32;
+        float height = 5.3f;
+        boolean married = true;
+        char sex = 'F';
+        double population = 4828853;
+        System.out.println("\n name: sridevi \n city:chennai \n age: 32 \n height: 5.3 \n married: true \n sex: f\n population:4828853" );
+	}
+} 
+ 
