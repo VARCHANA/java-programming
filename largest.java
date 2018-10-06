369a5ca4a53451a7d3e15aa0854c69d21b501c05
VARCHANA <43057131+VARCHANA@users.noreply.github.com>
2018-10-06 04:29:50 -0700

    Add files via upload

---
+++ largest.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {
+
+      int a = 1, b = 2, c =3 ;
+
+      if( 1 >= 2 && 1 >= 3)
+          System.out.println("1");
+
+      else if (2 >= 1 && 2 >= 3)
+          System.out.println("2");
+
+      else
+          System.out.println("3");
+  }
+}
+ 
