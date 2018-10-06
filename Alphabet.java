3dee8e8e34ed2ccfde23a8ee44d7b8cf43b65ad7
VARCHANA <43057131+VARCHANA@users.noreply.github.com>
2018-10-06 11:47:23 +0530

    Create Alphabet.java

---
+++ Alphabet.java
@@ -0,0 +1,12 @@
+public class Alphabet{
+
+     public static void main(String []args){
+         char x = 'a';
+
+        if( (x >= 'a' && x <= 'z') || (x >= 'A' && x <= 'Z'))
+            System.out.println(" Alphabet");
+        else
+            System.out.println( "no");
+        
+     }
+}
